b58f96c6d700fcf6c30826cd673a46bcaa70598b
phisac12 <isac_contest@hotmail.com>
2020-10-10 00:00:31 -0300

    Melhorei a segurança no form

--- form.php
+++ form.php
@@ -1,22 +1,37 @@
 <?php
+include_once 'conexao.php';
 
+if(empty($_POST['email1'])){
+    
+    echo "<script>window.location='../index.html';alert('$nome, você precisa preencher seu Email');</script>";
+    die;
+}
 
+function clear($input){
+    global $conexao;
+    //sql
+    $var = mysqli_escape_string($conexao, $input);
+    //xss (ou SQL INJECTION)
+    $var = htmlspecialchars($var);
+    return $var;
+    
+}
+
+if(isset($_POST['btn-entrar'])){
+    $nome = clear($_POST['nome1']);
+    $sobrenome = clear($_POST['sobrenome1']);
+    $email = clear($_POST['email1']);
+    $opcoes = clear($_POST['options1']);
+    
+}
+
+if(isset($_POST['btn-enviar'])){
+    
 $nome = $_POST['nome1'];
 $sobrenome = $_POST['sobrenome1'];
 $email = $_POST['email1'];
 $opcoes = $_POST['options1'];
-
-$servidor = 'localhost';
-$usuario  = 'root';
-$senhaServ = '';
-$banco = 'formsite';
-
-$conexao  = new mysqli($servidor, $usuario, $senhaServ, $banco);
-
-if ($conexao->connect_error){
-
-    die('Erro de conexão:' . $conexao->connect_error);
-
+    
 }
 
 $info = "INSERT INTO clientes (nome, sobrenome, email, options) VALUES ('$nome','$sobrenome', '$email', '$opcoes'
